19524f453b5560dab52d13084281054dced06417
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-27 18:31:15 -0400

    refactor: rewrite quick sort to be cleaner, and take user input

--- src/sorting/quick-sort.ts
+++ src/sorting/quick-sort.ts
@@ -1,38 +1,19 @@
-var input: any[];
-var testarray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Already sorted
-var testarray2 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]; // Reverse
-var testarray3 = [1, 3, 9, 4, 6, 2, 5, 8, 7, 10]; // Randomly set up
+import { configureInput } from "../configure-input";
 
-function quick_sort(inputarray: number[], start: number, end: number) {
-	if (start < end) {
-		let q = partition(inputarray, start, end);
-		quick_sort(inputarray, start, q - 1);
-		quick_sort(inputarray, q + 1, end);
-	}
+var targetarray = configureInput(process.argv)
+console.log('Final Result: ', quickSort(targetarray));
+
+function quickSort(arr: Array<number>) {
+	// Base case: Array is a single element. Return for bubbling.
+	if (arr.length <= 1) { return arr; }
+
+	let left = []; // Instantiate left and right side of the array.
+	let right = [];
+	const pivot = arr.pop() // Last item will be our pivot.
+
+	arr.forEach((num: number) => {
+		num > pivot ? right.push(num) : left.push(num);
+	});
+
+	return [...quickSort(left), pivot, ...quickSort(right)];
 }
-
-function partition(inputarray: number[], start: number, end: number) {
-	let x = inputarray[end]; // Last value in the array
-	let index = start;
-	for (let cursor = start; cursor <= end; cursor++) {
-		if (inputarray[cursor] <= x) {
-			let temp = inputarray[index]
-			inputarray[index] = inputarray[cursor];
-			inputarray[cursor] = temp;
-			index++
-		}
-	}
-	return index;
-}
-
-console.log("Entering with array: ", testarray1);
-quick_sort(testarray1, 0, testarray1.length - 1);
-console.log("After Sorting: ", testarray1);
-
-console.log("Entering with array: ", testarray2);
-quick_sort(testarray2, 0, testarray2.length - 1);
-console.log("After Sorting: ", testarray2);
-
-console.log("Entering with array: ", testarray3);
-quick_sort(testarray3, 0, testarray3.length - 1);
-console.log("After Sorting: ", testarray3);
