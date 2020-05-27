92b439e28e446a028346417b5605300b0da2a433
Robert Crowdis <RobertCrowdis@gmail.com>
2020-05-27 14:02:03 -0400

    fix: generate-array created 11 random numbers instead of 10

--- src/generate-array.ts
+++ src/generate-array.ts
@@ -1,6 +1,6 @@
 export function generateArray() {
 	const randomArray: number[] = [];
-	for (let i = 0; i <= 10; i++) {
+	for (let i = 0; i < 10; i++) {
 		randomArray.push(Math.round(Math.random() * 10));
 	}
 	return randomArray;
